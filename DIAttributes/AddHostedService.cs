20b0197b131dbd1c70db405382a4c609bd812277
Michel de Kok <info@micheldekok.nl>
2026-01-22 12:48:30 +0100

    # Include AddHostedService attribute and update packages.

---
+++ DIAttributes/AddHostedService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Mdk.DIAttributes;
+
+/// <summary>Dependency Injection Attribute for registering hosted service types.</summary>
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = false)]
+public class AddHostedService<HostedServiceType> : Attribute
+{
+    /// <summary>Gets the hosted service type registered.</summary>
+    public Type? ServiceType { get; } = typeof(HostedServiceType);
+}
